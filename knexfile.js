88b3b450a7c5aeb0d70dec09b7bbabb72978f122
Adam Horak <adam@thehoraks.com>
2014-06-19 13:54:58 -0700

    updated knex for heroku db config

--- knexfile.js
+++ knexfile.js
@@ -5,10 +5,10 @@ module.exports = {
   development: {
     client: 'postgres',
     connection: {
-      host     : process.env.APP_DB_HOST     || '127.0.0.1',
-      user     : process.env.APP_DB_USER     || '',
-      password : process.env.APP_DB_PASSWORD || '',
-      database : process.env.APP_DB_NAME     || 'peoplestore'
+      host     : process.env.APP_DB_HOST      || '127.0.0.1',
+      user     : process.env.APP_DB_USER      || '',
+      password : process.env.APP_DB_PASSWORD  || '',
+      database : process.env.DATABASE_URL     || 'peoplestore'
     }
   },
 
